e5940dcaff677b15f977eca91928071ee35e069c
Kirill Bobyrev <kbobyrev.opensource@gmail.com>
2021-04-10 23:34:10 +0300

    Start using zoxide

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,5 @@
 fish_vi_key_bindings
 set -gx LC_ALL en_US.UTF-8
-set -gx PATH /usr/local/google/home/kbobyrev/.cargo/bin /usr/lib/google-golang/bin:/usr/local/buildtools/java/jdk/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
 
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
@@ -9,3 +8,6 @@ if not functions -q fisher
 end
 
 set -g pure_symbol_prompt "λ"
+set -gx GOPATH /home/kirillbobyrev/go
+
+zoxide init fish | source
