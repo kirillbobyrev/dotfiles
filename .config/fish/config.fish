e8c9838e530bc2ac2a0577205a070d40ccc7aaec
Kirill Bobyrev <kbobyrev.opensource@gmail.com>
2021-04-25 17:12:15 +0300

    Update configurations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,5 @@
 fish_vi_key_bindings
 set -gx LC_ALL en_US.UTF-8
 
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
-
-set -g pure_symbol_prompt "λ"
-set -gx GOPATH /home/kirillbobyrev/go
-
 zoxide init fish | source
+starship init fish | source
